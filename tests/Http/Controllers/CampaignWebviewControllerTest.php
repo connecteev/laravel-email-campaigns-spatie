7b929c85b3698e1e47ea7da8319308a868040c39
freek <freek@spatie.be>
2019-10-06 13:25:07 +0200

    wip

---
+++ tests/Http/Controllers/CampaignWebviewControllerTest.php
@@ -0,0 +1,51 @@
+<?php
+
+
+namespace Spatie\EmailCampaigns\Tests\Http\Controllers;
+
+use Spatie\EmailCampaigns\Enums\CampaignStatus;
+use Spatie\EmailCampaigns\Http\Controllers\CampaignWebviewController;
+use Spatie\EmailCampaigns\Models\Campaign;
+use Spatie\EmailCampaigns\Tests\TestCase;
+
+class CampaignWebviewControllerTest extends TestCase
+{
+    /** @var \Spatie\EmailCampaigns\Models\Campaign */
+    private $campaign;
+
+    /** @var string */
+    private $webviewUrl;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->campaign = factory(Campaign::class)->create([
+            'webview_html' => 'my webview html',
+        ]);
+
+        $this->campaign->markAsSent(1);
+
+        $this->webviewUrl = action(CampaignWebviewController::class, $this->campaign->uuid);
+    }
+
+    /** @test */
+    public function it_can_display_the_webview_for_a_campaign()
+    {
+        $this
+            ->get($this->webviewUrl)
+            ->assertSuccessful()
+            ->assertSee('my webview html');
+    }
+
+    /** @test */
+    public function it_will_not_display_a_webview_for_a_campaign_that_has_not_been_sent()
+    {
+        $this->campaign->update(['status' => CampaignStatus::CREATED]);
+
+        $this
+            ->get($this->webviewUrl)
+            ->assertStatus(404);
+
+    }
+}
